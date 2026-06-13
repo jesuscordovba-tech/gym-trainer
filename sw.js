ac2070dcad79200f2fa0980ef875916331be2885
jesuscordovba-tech <jesuscordovba@gmail.com>
2026-06-13 17:56:28 -0500

    bump: cache v3 + script v=29 for cache refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gym-trainer-v2'
+const CACHE = 'gym-trainer-v3'
 const URLS = ['/', 'index.html', 'css/style.css', 'js/data.js', 'js/auth.js', 'js/db.js', 'js/app.js', 'manifest.json']
 
 self.addEventListener('install', e => {

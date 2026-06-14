04199000b090b40bb4bb35231df125bc8cfba1c2
jesuscordovba-tech <jesuscordovba@gmail.com>
2026-06-13 22:00:16 -0500

    fix: delete old SW caches, explicitly skip waiting

--- sw.js
+++ sw.js
@@ -8,6 +8,9 @@ self.addEventListener('install', e => {
 
 self.addEventListener('activate', e => {
   e.waitUntil(clients.claim())
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  )
 })
 
 self.addEventListener('fetch', e => {
